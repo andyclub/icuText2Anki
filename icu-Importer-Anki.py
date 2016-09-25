301901b8f0322972e1f51205087d086b40c62117
indyshao <shaofa@gmail.com>
2016-09-25 20:55:28 +0800

    fix a bug, and add the stdout chars

--- icu-Importer-Anki.py
+++ icu-Importer-Anki.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # encoding:utf-8
 
-Question,Answer = []
+Question = []
+Answer = []
 # Set the path to your own note address and note filename
 file =  open("/Users/ad/Downloads/Anki_ICU_Book.txt")
 line =' '
 while line :
     line = file.readline()
     # get Question
-    if 'Q:' in line and len(line) > 3:
+    if 'Q:' in line and len(line) > 4:
         # just support single line question
         line = line.replace('\n','')
         Question.append(line)
@@ -21,17 +22,18 @@ while line :
             while line.startswith('-'):
                 line = line.replace('-','   ',1)
             tmp = tmp + line
-        if len(tmp) > 3 :
+        if len(tmp.strip()) > 3 :
             Answer.append(tmp)
 file.close()
 # set the path to your own not address and a temp file name
 file = open('//Users/ad/Downloads/Anki_ICU_Book_tmp.csv','w')
 i = 0
+print 'Q&A writing into csv file, casds like this:'
 while i < len(Question):
-#    print Question[i]
-#    print Answer[i]
-#    print '...........'
-    csvfile.write('"' + Question[i]+'",')
-    csvfile.write('"' + Answer[i] + '"\n')
+    print Question[i]
+    print Answer[i]
+    print '...........'
+    file.write('"' + Question[i]+'",')
+    file.write('"' + Answer[i] + '"\n')
     i = i + 1
 file.close()
